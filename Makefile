9aef02b3a5315ec6650857f9122a8363832c35ce
sudorook <daemon@nullcodon.com>
2020-03-29 02:10:12 -0500

    add code for computing sequence energies

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 COMPUTE := compute_stats
 COMPARE := compare_stats
+ENERGY := compute_energies
+
 CC := g++
 
 CXXFLAGS = -O3 $(shell pkg-config --cflags armadillo)
@@ -19,9 +21,14 @@ SOURCES_COMPARE = ${SRCPATH}/compare_stats.cpp \
                   ${SRCPATH}/utils.cpp
 OBJECTS_COMPARE = $(SOURCES_COMPARE:%.cpp=%.o)
 
+SOURCES_ENERGY = ${SRCPATH}/compute_energies.cpp \
+                 ${SRCPATH}/msa.cpp \
+                 ${SRCPATH}/utils.cpp
+OBJECTS_ENERGY = $(SOURCES_ENERGY:%.cpp=%.o)
+
 .PHONY: all
 
-all: $(COMPUTE) $(COMPARE)
+all: $(COMPUTE) $(COMPARE) $(ENERGY)
 
 $(COMPUTE): $(OBJECTS_COMPUTE)
 	$(CC) $(CXXFLAGS) -o $@ $^ $(LDFLAGS)
@@ -29,10 +36,15 @@ $(COMPUTE): $(OBJECTS_COMPUTE)
 $(COMPARE): $(OBJECTS_COMPARE)
 	$(CC) $(CXXFLAGS) -o $@ $^ $(LDFLAGS)
 
+$(ENERGY): $(OBJECTS_ENERGY)
+	$(CC) $(CXXFLAGS) -o $@ $^ $(LDFLAGS)
+
 .PHONY: clean
 
 clean:
 	rm -f $(COMPUTE)
 	rm -f $(COMPARE)
+	rm -f $(ENERGY)
 	rm -f $(OBJECTS_COMPUTE)
 	rm -f $(OBJECTS_COMPARE)
+	rm -f $(OBJECTS_ENERGY)
---
+++ src/compute_energies.cpp
@@ -0,0 +1,88 @@
+#include <armadillo>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <vector>
+
+#include "msa.hpp"
+#include "utils.hpp"
+
+int main(int argc, char* argv[]) {
+  std::string msa_file;
+  std::string energy_file = "seqeuence_energies.txt";
+  std::string params_file;
+  std::string params_J_file;
+  // std::string dest = ".";
+  bool compat_mode = true;
+
+  char c;
+  while ((c = getopt(argc, argv, "i:o:p:P:")) != -1) {
+    switch (c) {
+      case 'i':
+        msa_file = optarg;
+        break;
+      case 'o':
+        energy_file = optarg;
+        break;
+      // case 'd':
+      //   dest = optarg;
+      //   break;
+      case 'p':
+        params_file = optarg;
+        break;
+      case 'P':
+        params_J_file = optarg;
+        compat_mode = false;
+        break;
+      // case 'r':
+      //   reweight = true;
+      //   break;
+      case '?':
+        std::cerr << "what the fuck?" << std::endl;
+    }
+  }
+
+  int idx = msa_file.find_last_of("."); 
+  std::string prefix = msa_file.substr(0, idx);
+
+  std::cout << "reading sequences... " << std::flush;
+  MSA msa = MSA(msa_file, true, true, 0.8);
+  std::cout << "done" << std::endl;
+
+  std::cout << "reading parameters... " << std::flush;
+  potts_model params;
+  if (compat_mode) {
+    params = loadPottsModelCompat(params_file);
+  } else {
+    params = loadPottsModel(params_file, params_J_file);
+  }
+  std::cout << "done" << std::endl;
+
+  std::cout << "computing energies... " << std::flush;
+  arma::Mat<double> energies = arma::Mat<double>(1, msa.M, arma::fill::zeros);
+  double E;
+  for (int rep = 0; rep < 1; rep++) {
+    for (int seq = 0; seq < msa.M; seq++) {
+      E = 0;
+      for (int i = 0; i < msa.N; i++) {
+        E -= params.h.at(msa.alignment.at(seq, i), i);
+        for (int j = i + 1; j < msa.N; j++) {
+          E -= params.J.at(i, j).at(msa.alignment.at(seq, i),
+                                    msa.alignment.at(seq, j));
+        }
+      }
+      energies.at(rep, seq) = E;
+    }
+  }
+  std::cout << "done" << std::endl;
+
+  std::ofstream output_stream(energy_file);
+
+  for (int rep = 0; rep < 1; rep++) {
+    for (int m = 0; m < msa.M; m++) {
+      output_stream << energies.at(rep, m) << std::endl;
+    }
+  }
+}
