c1e1a61b56c46b55371e7e2073b4415026883e68
sudorook <daemon@nullcodon.com>
2020-03-29 02:59:58 -0500

    add cli parser for compare_stats.sh

--- compare_stats.sh
+++ compare_stats.sh
@@ -3,10 +3,39 @@ set -eu
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-MSA="$1"
-MCMC="$2"
-DESCRIPTION="$3"
-THRESHOLD="$4"
+OPTIONS=s:c:d:t:
+LONGOPTIONS=msa:,mcmc:,description:,threshold:
+PARSED=$(getopt -o ${OPTIONS} --long ${LONGOPTIONS} -n "$0" -- "$@")
+eval set -- "$PARSED"
+
+while [ $# -ge 1 ]; do
+  case "$1" in
+    -s|--msa)
+      MSA="$2"
+      shift 2
+      ;;
+    -c|--mcmc)
+      MCMC="$2"
+      shift 2
+      ;;
+    -d|--description)
+      DESCRIPTION="$2"
+      shift 2
+      ;;
+    -t|--threshold)
+      THRESHOLD="$2"
+      shift 2
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo "DANGER, WILL ROBINSON!"
+      exit 3
+      ;;
+  esac
+done
 
 SLUG=$(echo "$DESCRIPTION" | tr '[:upper:]' '[:lower:]' | sed -e "s/ /_/g")
 
