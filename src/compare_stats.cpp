7973b2dbaf93a78908abd46c84d97d0491fcc1c8
sudorook <daemon@nullcodon.com>
2020-04-05 00:24:41 -0500

    don't rely on AA_ALPHABET_SIZE when Q known already

--- src/compare_stats.cpp
+++ src/compare_stats.cpp
@@ -9,10 +9,6 @@
 #include "msa.hpp"
 #include "msa_stats.hpp"
 
-#ifndef AA_ALPHABET_SIZE
-#define AA_ALPHABET_SIZE 21
-#endif
-
 int main(int argc, char* argv[]) {
   std::string msa_file;
   std::string mc_file;
@@ -43,9 +39,6 @@ int main(int argc, char* argv[]) {
   std::string msa_prefix = msa_file.substr(0, idx);
   idx = mc_file.find_last_of(".");
   std::string mc_prefix = mc_file.substr(0, idx);
-
-  // msa.writeSequenceWeights(msa_prefix + "_weights.txt");
-  // mc.writeSequenceWeights(mc_prefix + "_weights.txt");
   
   std::cout << "computing stats" << std::endl;
   MSAStats msa_stats = MSAStats(msa);
@@ -65,15 +58,16 @@ int main(int argc, char* argv[]) {
     std::ofstream mc_stream(mc_prefix + "_corr_3p.txt");
 
     int N = msa.N;
+    int Q = msa.Q;
 
     double msa_tmp = 0;
     double mc_tmp = 0;
     for (int i = 0; i < N; i++) {
       for (int j = i + 1; j < N; j++) {
         for (int k = j + 1; k < N; k++) {
-          for (int aa1 = 0; aa1 < AA_ALPHABET_SIZE; aa1++) {
-            for (int aa2 = 0; aa2 < AA_ALPHABET_SIZE; aa2++) {
-              for (int aa3 = 0; aa3 < AA_ALPHABET_SIZE; aa3++) {
+          for (int aa1 = 0; aa1 < Q; aa1++) {
+            for (int aa2 = 0; aa2 < Q; aa2++) {
+              for (int aa3 = 0; aa3 < Q; aa3++) {
                 msa_tmp = msa_stats.frequency_3p.at(i, j, k).at(aa1, aa2, aa3) -
                           msa_stats.frequency_2p.at(i, j).at(aa1, aa2) *
                             msa_stats.frequency_1p.at(aa3, k) -
@@ -95,7 +89,6 @@ int main(int argc, char* argv[]) {
                            mc_stats.frequency_1p.at(aa2, j) *
                            mc_stats.frequency_1p.at(aa3, k);
                 if ((fabs(msa_tmp) > threshold) || (fabs(mc_tmp) > threshold)) {
-                // if ((fabs(msa_tmp) > threshold) && (fabs(mc_tmp) > threshold)) {
                   msa_stream << msa_tmp << std::endl;
                   mc_stream << mc_tmp << std::endl;
                 }
--- src/msa_stats.cpp
+++ src/msa_stats.cpp
@@ -5,24 +5,20 @@
 #include <fstream>
 #include <iostream>
 
-#ifndef AA_ALPHABET_SIZE
-#define AA_ALPHABET_SIZE 21
-#endif
-
 MSAStats::MSAStats(MSA msa)
 {
   // Initialize
   N = msa.N;
   M = msa.M;
-  Q = AA_ALPHABET_SIZE;
+  Q = msa.Q;
 
-  frequency_1p = arma::Mat<double>(AA_ALPHABET_SIZE, N, arma::fill::zeros);
+  frequency_1p = arma::Mat<double>(Q, N, arma::fill::zeros);
   frequency_2p = arma::field<arma::Mat<double>>(N, N);
   frequency_3p = arma::field<arma::Cube<double>>(N, N, N);
   rel_entropy_grad_1p =
-    arma::Mat<double>(AA_ALPHABET_SIZE, N, arma::fill::zeros);
+    arma::Mat<double>(Q, N, arma::fill::zeros);
   aa_background_frequencies =
-    arma::Col<double>::fixed<AA_ALPHABET_SIZE>(arma::fill::zeros);
+    arma::Col<double>(Q, arma::fill::ones);
 
   aa_background_frequencies = {
     0.000, 0.073, 0.025, 0.050, 0.061, 0.042, 0.072, 0.023, 0.053, 0.064, 0.089,
@@ -48,8 +44,7 @@ MSAStats::MSAStats(MSA msa)
   // Compute the 2p statistics
   for (int i = 0; i < N; i++) {
     for (int j = i + 1; j < N; j++) {
-      frequency_2p.at(i, j) = arma::Mat<double>(
-        AA_ALPHABET_SIZE, AA_ALPHABET_SIZE, arma::fill::zeros);
+      frequency_2p.at(i, j) = arma::Mat<double>(Q, Q, arma::fill::zeros);
 
       int* align_ptr1 = msa.alignment.colptr(i);
       int* align_ptr2 = msa.alignment.colptr(j);
@@ -65,8 +60,8 @@ MSAStats::MSAStats(MSA msa)
   for (int i = 0; i < N; i++) {
     for (int j = i + 1; j < N; j++) {
       for (int k = j + 1; k < N; k++) {
-        frequency_3p.at(i, j, k) = arma::Cube<double>(
-          AA_ALPHABET_SIZE, AA_ALPHABET_SIZE, AA_ALPHABET_SIZE, arma::fill::zeros);
+        frequency_3p.at(i, j, k) =
+          arma::Cube<double>(Q, Q, Q, arma::fill::zeros);
 
         int* align_ptr1 = msa.alignment.colptr(i);
         int* align_ptr2 = msa.alignment.colptr(j);
@@ -147,7 +142,7 @@ MSAStats::writeRelEntropyGradient(std::string output_file)
   std::ofstream output_stream(output_file);
 
   for (int i = 0; i < N; i++) {
-    for (int aa = 0; aa < AA_ALPHABET_SIZE; aa++) {
+    for (int aa = 0; aa < Q; aa++) {
       output_stream << i << " " << aa << " " << rel_entropy_grad_1p.at(aa, i)
                     << std::endl;
     }
@@ -160,7 +155,7 @@ MSAStats::writeFrequency1p(std::string output_file)
   std::ofstream output_stream(output_file);
 
   for (int i = 0; i < N; i++) {
-    for (int aa = 0; aa < AA_ALPHABET_SIZE; aa++) {
+    for (int aa = 0; aa < Q; aa++) {
       output_stream << frequency_1p.at(aa, i) << std::endl;
     }
   }
@@ -173,13 +168,11 @@ MSAStats::writeFrequency2p(std::string output_file)
 
   for (int i = 0; i < N; i++) {
     for (int j = i + 1; j < N; j++) {
-      output_stream << i << " " << j;
-      for (int aa1 = 0; aa1 < AA_ALPHABET_SIZE; aa1++) {
-        for (int aa2 = 0; aa2 < AA_ALPHABET_SIZE; aa2++) {
-          output_stream << " " << frequency_2p.at(i, j).at(aa1, aa2);
+      for (int aa1 = 0; aa1 < Q; aa1++) {
+        for (int aa2 = 0; aa2 < Q; aa2++) {
+          output_stream  << frequency_2p.at(i, j).at(aa1, aa2) << std::endl;
         }
       }
-      output_stream << std::endl;
     }
   }
 };
@@ -192,9 +185,9 @@ MSAStats::writeFrequency3p(std::string output_file)
   for (int i = 0; i < N; i++) {
     for (int j = i + 1; j < N; j++) {
       for (int k = j + 1; k < N; k++) {
-        for (int aa1 = 0; aa1 < AA_ALPHABET_SIZE; aa1++) {
-          for (int aa2 = 0; aa2 < AA_ALPHABET_SIZE; aa2++) {
-            for (int aa3 = 0; aa3 < AA_ALPHABET_SIZE; aa3++) {
+        for (int aa1 = 0; aa1 < Q; aa1++) {
+          for (int aa2 = 0; aa2 < Q; aa2++) {
+            for (int aa3 = 0; aa3 < Q; aa3++) {
               output_stream << frequency_3p.at(i, j, k).at(aa1, aa2, aa3) <<
                 std::endl;
             }
@@ -212,8 +205,8 @@ MSAStats::writeCorrelation2p(std::string output_file)
 
   for (int i = 0; i < N; i++) {
     for (int j = i + 1; j < N; j++) {
-      for (int aa1 = 0; aa1 < AA_ALPHABET_SIZE; aa1++) {
-        for (int aa2 = 0; aa2 < AA_ALPHABET_SIZE; aa2++) {
+      for (int aa1 = 0; aa1 < Q; aa1++) {
+        for (int aa2 = 0; aa2 < Q; aa2++) {
           output_stream << frequency_2p.at(i, j).at(aa1, aa2) -
             frequency_1p.at(aa1, i)*frequency_1p.at(aa2, j) << std::endl;
         }
@@ -230,9 +223,9 @@ MSAStats::writeCorrelation3p(std::string output_file)
   for (int i = 0; i < N; i++) {
     for (int j = i + 1; j < N; j++) {
       for (int k = j + 1; k < N; k++) {
-        for (int aa1 = 0; aa1 < AA_ALPHABET_SIZE; aa1++) {
-          for (int aa2 = 0; aa2 < AA_ALPHABET_SIZE; aa2++) {
-            for (int aa3 = 0; aa3 < AA_ALPHABET_SIZE; aa3++) {
+        for (int aa1 = 0; aa1 < Q; aa1++) {
+          for (int aa2 = 0; aa2 < Q; aa2++) {
+            for (int aa3 = 0; aa3 < Q; aa3++) {
               output_stream << frequency_3p.at(i, j, k).at(aa1, aa2, aa3) -
                 frequency_2p.at(i, j).at(aa1, aa2)*frequency_1p.at(aa3, k) -
                 frequency_2p.at(i, k).at(aa1, aa3)*frequency_1p.at(aa2, j) -
@@ -256,9 +249,9 @@ MSAStats::writeCorrelation3p(std::string output_file)
 //   for (int i = 0; i < N; i++) {
 //     for (int j = i + 1; j < N; j++) {
 //       for (int k = j + 1; k < N; k++) {
-//         for (int aa1 = 0; aa1 < AA_ALPHABET_SIZE; aa1++) {
-//           for (int aa2 = 0; aa2 < AA_ALPHABET_SIZE; aa2++) {
-//             for (int aa3 = 0; aa3 < AA_ALPHABET_SIZE; aa3++) {
+//         for (int aa1 = 0; aa1 < Q; aa1++) {
+//           for (int aa2 = 0; aa2 < Q; aa2++) {
+//             for (int aa3 = 0; aa3 < Q; aa3++) {
 //               tmp =
 //                 frequency_3p.at(i, j, k).at(aa1, aa2, aa3) -
 //                 frequency_2p.at(i, j).at(aa1, aa2) * frequency_1p.at(aa3, k) -
