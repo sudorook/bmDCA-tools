8fe80af2df8c8dc3576e2b97144ce6ae5a7e2a5a
sudorook <daemon@nullcodon.com>
2020-04-06 21:19:55 -0500

    redo plot_J, and plot h along diagonal

--- plot_J.py
+++ plot_J.py
@@ -36,24 +36,40 @@ def reduce_tensor(J, norm=2):
     return J_new
 
 
+def reduce_h(h):
+    """ reduce rank 4 tensor using 2-norm """
+    Npos = np.shape(h)[0]
+    Naa = np.shape(h)[1]
+    h_new = np.zeros(Npos)
+    for i in range(Npos):
+        for j in range(Naa):
+            h_new[i] += (h[i, j])**2
+        h_new[i] = h_new[i]**.5
+    return h_new
+
+
 def load_data(data_file):
     """ syntax checker is annoying me """
 
     tmpJ = []
     tmph = []
+    Naa = 0
+    Npos = 0
     with open(data_file, "r") as handle:
         for line in handle:
             if line[0] == "J":
                 tmpJ.append(float(line.split(" ")[5].strip()))
             elif line[0] == "h":
+                tmp = line.split(" ")
                 tmph.append(float(line.split(" ")[3].strip()))
+                if tmp[1] == "0":
+                    Naa += 1
+                if tmp[2] == "0":
+                    Npos += 1
 
     tmpJ = np.array(tmpJ)
     tmph = np.array(tmph)
 
-    #  h = np.loadtxt(h_file)
-    Naa = 21
-    Npos = int(len(tmph) / Naa)
     h = tmph.reshape(Npos, Naa)
 
     # Jijs are saved as upper triangular matrix...
@@ -75,13 +91,12 @@ def main():
     prefix = os.path.splitext((options.params).replace("/", "_"))[0]
 
     h, J = load_data(options.params)
-    #  J_2d = reduce_tensor(J)
     J_2d = reduce_tensor(J, "fro")
-    J_2d = J_2d + np.transpose(J_2d)
-    print(J_2d)
+    h_1d = reduce_h(h)
+    model = J_2d + np.transpose(J_2d) + np.diag(h_1d)
 
     # Make plots
-    ax = sns.heatmap(J_2d, cmap="Blues", linewidths=0.25, vmin=0, vmax=2)
+    ax = sns.heatmap(model, cmap="Blues", linewidths=0.25, vmin=0, vmax=2)
     fig = ax.get_figure()
     ax.xaxis.set_ticks_position("top")
     ax.yaxis.set_ticks_position("left")
