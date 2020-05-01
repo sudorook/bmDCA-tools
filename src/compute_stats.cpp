799da617808fcaf962969e54bb227eceac3da81e
sudorook <daemon@nullcodon.com>
2020-05-01 16:40:19 -0500

    update the cli args and output names for compute_stats

--- src/compute_stats.cpp
+++ src/compute_stats.cpp
@@ -11,32 +11,34 @@
 
 int main(int argc, char* argv[]) {
   std::string infile;
-  std::string outfile;
-  std::string dest;
-  // bool reweight = true;
+  bool reweight = false;
+  bool is_numeric = false;
+  double threshold = 0.8;
 
   char c;
-  while ((c = getopt(argc, argv, "i:o:")) != -1) {
+  while ((c = getopt(argc, argv, "i:n:rt:")) != -1) {
     switch (c) {
       case 'i':
         infile = optarg;
+        is_numeric = false;
         break;
-      case 'o':
-        outfile = optarg;
+      case 'n':
+        infile = optarg;
+        is_numeric = true;
         break;
-      // case 'd':
-      //   dest = optarg;
-      //   break;
-      // case 'r':
-      //   reweight = true;
-      //   break;
+      case 'r':
+        reweight = true;
+        break;
+      case 't':
+        threshold = std::stod(optarg);
+        break;
       case '?':
         std::cerr << "what the fuck?" << std::endl;
     }
   }
 
   std::cout << "reading sequences" << std::endl;
-  MSA msa = MSA(infile, true, true, 0.8);
+  MSA msa = MSA(infile, reweight, is_numeric, threshold);
 
   int idx = infile.find_last_of("."); 
   std::string prefix = infile.substr(0, idx);
@@ -46,12 +48,14 @@ int main(int argc, char* argv[]) {
   MSAStats msa_stats = MSAStats(msa);
   
   std::cout << "writing 1p stats" << std::endl;
-  msa_stats.writeFrequency1p(outfile + "_freq_1p.txt");
+  msa_stats.writeFrequency1p(prefix + "_freq_1p.bin");
   
   std::cout << "writing 2p stats" << std::endl;
-  msa_stats.writeCorrelation2p(outfile + "_corr_2p.txt");
+  msa_stats.writeFrequency2p(prefix + "_freq_2p.bin");
+  msa_stats.writeCorrelation2p(prefix + "_corr_2p.bin");
   
-  std::cout << "writing 3p stats" << std::endl;
-  // msa_stats.writeCorrelation3p(outfile + "_corr_3p.txt", 0.01);
-  msa_stats.writeCorrelation3p(outfile + "_corr_3p.txt");
+  // std::cout << "writing 3p stats" << std::endl;
+  // msa_stats.writeFrequency3p(prefix + "_freq_3p.bin");
+  // msa_stats.writeFrequency3pAscii(prefix + "_freq_3p.txt");
+  // msa_stats.writeCorrelation3p(prefix + "_corr_3p.bin");
 }
