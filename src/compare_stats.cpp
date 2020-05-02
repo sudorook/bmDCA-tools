9dcd701a8475a9b489b2fe359f8f67e7285261d4
sudorook <daemon@nullcodon.com>
2020-05-02 16:53:25 -0500

    write histrograms and plot them

--- src/compare_stats.cpp
+++ src/compare_stats.cpp
@@ -9,6 +9,51 @@
 #include "msa.hpp"
 #include "msa_stats.hpp"
 
+#define BINS 201
+
+typedef struct {
+  arma::Mat<unsigned long long int> grid;
+  int bins;
+  double bin_width;
+  double min=0;
+  double max=1;
+} histogram;
+
+typedef struct {
+  double a;
+  double b;
+  double R2;
+} linear_model;
+
+void
+writeHistogram(std::string file, histogram hist)
+{
+  std::ofstream output_stream(file);
+  for (int i = 0; i < BINS; i++) {
+    for (int j = 0; j < BINS; j++) {
+      output_stream << hist.min + i * hist.bin_width << "\t"
+                    << hist.min + j * hist.bin_width << "\t"
+                    << hist.grid.at(i, j) << std::endl;
+    }
+  }
+  output_stream.close();
+  return;
+};
+
+void
+writeLinearModel(std::string file, linear_model model)
+{
+  std::ofstream output_stream(file);
+  output_stream << "a"
+                << "\t" << model.a << std::endl;
+  output_stream << "b"
+                << "\t" << model.b << std::endl;
+  output_stream << "R2"
+                << "\t" << model.R2 << std::endl;
+  output_stream.close();
+  return;
+};
+
 int main(int argc, char* argv[]) {
   std::string msa_file;
   std::string mc_file;
@@ -48,6 +93,7 @@ int main(int argc, char* argv[]) {
   std::cout << "computing stats" << std::endl;
   MSAStats msa_stats = MSAStats(msa);
   MSAStats mc_stats = MSAStats(mc);
+  std::cout << std::endl;
 
   int N = msa_stats.getN();
   int Q = msa_stats.getQ();
@@ -64,7 +110,7 @@ int main(int argc, char* argv[]) {
 
   arma::wall_clock timer;
 
-  std::cout << "computing 1p frequency regression.." << std::endl;
+  std::cout << "computing 1p frequency regression... " << std::flush;
   timer.tic();
   {
     double f = 1. / (double)Q;
@@ -76,11 +122,31 @@ int main(int argc, char* argv[]) {
     double b = xy / xx;
     double a = f*(1-b);
     double r2 = (2*b*xy - b*b*xx)/yy;
-    std::cout << "model: " << b << " x + " << a << ", r2=" << r2 << std::endl;
-  }
-  std::cout << timer.toc() << " sec" << std::endl;
+    std::cout << timer.toc() << " sec" << std::endl;
+    std::cout << "model: " << b << " x + " << a << ", R^2=" << r2
+              << " (r=" << sqrt(r2) << ")" << std::endl;
 
-  std::cout << "computing 2p frequency regression.." << std::endl;
+    linear_model model;
+    model.a = a;
+    model.b = b;
+    model.R2 = r2;
+
+    histogram hist;
+    hist.grid =
+      arma::Mat<unsigned long long int>(BINS, BINS, arma::fill::zeros);
+    double bin_width = 1. / (double)(BINS - 1);
+    hist.bin_width = bin_width;
+    for (int i = 0; i < Q * N; i++) {
+      hist.grid(floor(msa_stats.frequency_1p(i) / bin_width),
+           floor(mc_stats.frequency_1p(i) / bin_width))++;
+    }
+    // hist.grid.print("freq 1p:");
+    writeHistogram("freq_1p_hist.tsv", hist);
+    writeLinearModel("freq_1p_model.tsv", model);
+    std::cout << std::endl;
+  }
+
+  std::cout << "computing 2p frequency regression... " << std::flush;
   timer.tic();
   {
     double f = 1. / (double)(Q*Q);
@@ -92,11 +158,32 @@ int main(int argc, char* argv[]) {
     double b = xy / xx;
     double a = f*(1-b);
     double r2 = (2*b*xy - b*b*xx)/yy;
-    std::cout << "model: " << b << " x + " << a << ", r2=" << r2 << std::endl;
-  }
-  std::cout << timer.toc() << " sec" << std::endl;
+    std::cout << timer.toc() << " sec" << std::endl;
+    std::cout << "model: " << b << " x + " << a << ", R^2=" << r2
+              << " (r=" << sqrt(r2) << ")"
+              << std::endl;
 
-  std::cout << "computing 2p correlation regression.." << std::endl;
+    linear_model model;
+    model.a = a;
+    model.b = b;
+    model.R2 = r2;
+
+    histogram hist;
+    hist.grid =
+      arma::Mat<unsigned long long int>(BINS, BINS, arma::fill::zeros);
+    double bin_width = 1. / (double)(BINS - 1);
+    hist.bin_width = bin_width;
+    for (int i = 0; i < N * (N - 1) / 2 * Q * Q; i++) {
+      hist.grid(floor(msa_stats.frequency_2p(i) / bin_width),
+           floor(mc_stats.frequency_2p(i) / bin_width))++;
+    }
+    // hist.grid.print("freq 2p:");
+    writeHistogram("freq_2p_hist.tsv", hist);
+    writeLinearModel("freq_2p_model.tsv", model);
+    std::cout << std::endl;
+  }
+
+  std::cout << "computing 2p correlation regression... " << std::flush;
   timer.tic();
   {
     double xy = arma::accu((msa_stats.correlation_2p) %
@@ -107,12 +194,38 @@ int main(int argc, char* argv[]) {
     double b = xy / xx;
     double a = 0;
     double r2 = (2*b*xy - b*b*xx)/yy;
-    std::cout << "model: " << b << " x + " << a << ", r2=" << r2 << std::endl;
-  }
-  std::cout << timer.toc() << " sec" << std::endl;
+    std::cout << timer.toc() << " sec" << std::endl;
+    std::cout << "model: " << b << " x + " << a << ", R^2=" << r2
+              << " (r=" << sqrt(r2) << ")" << std::endl;
 
-  std::cout << "computing 3p frequency and correlation regressions.."
-            << std::endl;
+    linear_model model;
+    model.a = a;
+    model.b = b;
+    model.R2 = r2;
+
+    histogram hist;
+    hist.grid =
+      arma::Mat<unsigned long long int>(BINS, BINS, arma::fill::zeros);
+    double max = std::max(arma::max(msa_stats.correlation_2p),
+                     arma::max(mc_stats.correlation_2p));
+    double min = std::min(arma::min(msa_stats.correlation_2p),
+                     arma::min(mc_stats.correlation_2p));
+    double bin_width = 1.05 * (max - min) / (double)(BINS - 1);
+    hist.bin_width = bin_width;
+    hist.max = 1.05 * max;
+    hist.min = 1.05 * min;
+    for (int i = 0; i < N * (N - 1) / 2 * Q * Q; i++) {
+      hist.grid(floor((msa_stats.correlation_2p(i) - hist.min) / bin_width + .5),
+                floor((mc_stats.correlation_2p(i) - hist.min) / bin_width + .5))++;
+    }
+    // hist.grid.print("corr 2p:");
+    writeHistogram("corr_2p_hist.tsv", hist);
+    writeLinearModel("corr_2p_model.tsv", model);
+    std::cout << std::endl;
+  }
+
+  std::cout << "computing 3p frequency and correlation regressions... "
+            << std::flush;
   timer.tic();
   {
     arma::Col<double> freq_xx_vec =
@@ -130,6 +243,34 @@ int main(int argc, char* argv[]) {
       arma::Col<double>((int)(N * (N - 1) * (N - 2) / 6), arma::fill::zeros);
 
     double f = 1. / (double)(Q*Q*Q);
+
+    double freq_bin_width = 1. / (double)(BINS - 1);
+    double max = std::max(arma::max(msa_stats.correlation_2p),
+                     arma::max(mc_stats.correlation_2p));
+    double min = std::min(arma::min(msa_stats.correlation_2p),
+                     arma::min(mc_stats.correlation_2p));
+    double corr_bin_width = (max - min) / (double)(BINS - 1);
+    // int offset = (int)((BINS - 1) / 2);
+
+    arma::Cube<unsigned long long int> freq_hist_i =
+      arma::Cube<unsigned long long int>(BINS, BINS, N, arma::fill::zeros);
+    arma::Cube<unsigned long long int> corr_hist_i =
+      arma::Cube<unsigned long long int>(BINS, BINS, N, arma::fill::zeros);
+
+    histogram freq_hist;
+    freq_hist.grid =
+      arma::Mat<unsigned long long int>(BINS, BINS, arma::fill::zeros);
+    freq_hist.bin_width = freq_bin_width;
+    freq_hist.bins = BINS;
+
+    histogram corr_hist;
+    corr_hist.grid =
+      arma::Mat<unsigned long long int>(BINS, BINS, arma::fill::zeros);
+    corr_hist.bin_width = corr_bin_width;
+    corr_hist.bins = BINS;
+    // corr_hist.offset = offset;
+    corr_hist.max = max;
+    corr_hist.min = min;
 
 #pragma omp parallel
     {
@@ -189,17 +330,19 @@ int main(int argc, char* argv[]) {
             for (int aa1 = 0; aa1 < Q; aa1++) {
               for (int aa2 = 0; aa2 < Q; aa2++) {
                 for (int aa3 = 0; aa3 < Q; aa3++) {
-                  int idx =
-                    aa3 + aa2 * Q + aa1 * Q * Q;
-                  int idx2_12 = aa2 + aa1 * Q +
-                                Q * Q * (j - i - 1 + i * (N - 1) - i * (i - 1) / 2);
-                  int idx2_13 = aa3 + aa1 * Q +
-                                Q * Q * (k - i - 1 + i * (N - 1) - i * (i - 1) / 2);
-                  int idx2_23 = aa3 + aa2 * Q +
-                                Q * Q * (k - j - 1 + j * (N - 1) - j * (j - 1) / 2);
+                  int idx_aa = aa3 + aa2 * Q + aa1 * Q * Q;
+                  int idx2_12 =
+                    aa2 + aa1 * Q +
+                    Q * Q * (j - i - 1 + i * (N - 1) - i * (i - 1) / 2);
+                  int idx2_13 =
+                    aa3 + aa1 * Q +
+                    Q * Q * (k - i - 1 + i * (N - 1) - i * (i - 1) / 2);
+                  int idx2_23 =
+                    aa3 + aa2 * Q +
+                    Q * Q * (k - j - 1 + j * (N - 1) - j * (j - 1) / 2);
 
-                  msa_correlation_3p_ijk.at(idx) =
-                    msa_frequency_3p_ijk.at(idx) -
+                  msa_correlation_3p_ijk.at(idx_aa) =
+                    msa_frequency_3p_ijk.at(idx_aa) -
                     msa_stats.frequency_2p.at(idx2_12) *
                       msa_stats.frequency_1p.at(aa3 + k * Q) -
                     msa_stats.frequency_2p.at(idx2_13) *
@@ -210,8 +353,8 @@ int main(int argc, char* argv[]) {
                       msa_stats.frequency_1p.at(aa2 + j * Q) *
                       msa_stats.frequency_1p.at(aa3 + k * Q);
 
-                  mc_correlation_3p_ijk.at(idx) =
-                    mc_frequency_3p_ijk.at(idx) -
+                  mc_correlation_3p_ijk.at(idx_aa) =
+                    mc_frequency_3p_ijk.at(idx_aa) -
                     mc_stats.frequency_2p.at(idx2_12) *
                       mc_stats.frequency_1p.at(aa3 + k * Q) -
                     mc_stats.frequency_2p.at(idx2_13) *
@@ -222,6 +365,27 @@ int main(int argc, char* argv[]) {
                       mc_stats.frequency_1p.at(aa2 + j * Q) *
                       mc_stats.frequency_1p.at(aa3 + k * Q);
                 }
+              }
+            }
+
+            {
+              arma::Col<double> tmp_msa_frequency_3p_ijk =
+                arma::floor(msa_frequency_3p_ijk / freq_bin_width);
+              arma::Col<double> tmp_mc_frequency_3p_ijk =
+                arma::floor(mc_frequency_3p_ijk / freq_bin_width);
+
+              arma::Col<double> tmp_msa_correlation_3p_ijk = arma::floor(
+                (msa_correlation_3p_ijk - min) / corr_bin_width + .5);
+              arma::Col<double> tmp_mc_correlation_3p_ijk = arma::floor(
+                (mc_correlation_3p_ijk - min) / corr_bin_width + .5);
+
+              for (int q = 0; q < Q * Q * Q; q++) {
+                freq_hist_i(tmp_msa_frequency_3p_ijk.at(q),
+                            tmp_mc_frequency_3p_ijk.at(q),
+                            i)++;
+                corr_hist_i(tmp_msa_correlation_3p_ijk.at(q),
+                            tmp_mc_correlation_3p_ijk.at(q),
+                            i)++;
               }
             }
 
@@ -236,6 +400,7 @@ int main(int argc, char* argv[]) {
       }
     }
     std::cout << timer.toc() << " sec" << std::endl;
+
     {
       double xx = arma::accu(freq_xx_vec);
       double xy = arma::accu(freq_xy_vec);
@@ -244,7 +409,19 @@ int main(int argc, char* argv[]) {
       double b = xy / xx;
       double a = f*(1-b);
       double r2 = (2*b*xy - b*b*xx)/yy;
-      std::cout << "model: " << b << " x + " << a << ", r2=" << r2 << std::endl;
+      std::cout << "model: " << b << " x + " << a << ", R^2=" << r2
+                << " (r=" << sqrt(r2) << ")" << std::endl;
+
+      linear_model model;
+      model.a = a;
+      model.b = b;
+      model.R2 = r2;
+
+      freq_hist.grid = arma::sum(freq_hist_i, 2);
+      // freq_hist.grid.print("freq 3p:");
+      writeHistogram("freq_3p_hist.tsv", freq_hist);
+      writeLinearModel("freq_3p_model.tsv", model);
+      std::cout << std::endl;
     }
     {
       double xx = arma::accu(corr_xx_vec);
@@ -254,8 +431,18 @@ int main(int argc, char* argv[]) {
       double b = xy / xx;
       double a = 0;
       double r2 = (2*b*xy - b*b*xx)/yy;
-      std::cout << "model: " << b << " x + " << a << ", r2=" << r2 << std::endl;
+      std::cout << "model: " << b << " x + " << a << ", R^2=" << r2
+                << " (r=" << sqrt(r2) << ")" << std::endl;
+
+      linear_model model;
+      model.a = a;
+      model.b = b;
+      model.R2 = r2;
+
+      corr_hist.grid = arma::sum(corr_hist_i, 2);
+      // corr_hist.grid.print("corr 3p:");
+      writeHistogram("corr_3p_hist.tsv", corr_hist);
+      writeLinearModel("corr_3p_model.tsv", model);
     }
   }
-  std::cout << timer.toc() << " sec" << std::endl;
 };
