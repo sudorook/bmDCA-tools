ca551d46589a3ac4113ef4bc606ba2f1d639153f
sudorook <daemon@nullcodon.com>
2020-05-05 17:14:44 -0500

    refactor energy histograms and consolidate functions

--- plot_energy.py
+++ plot_energy.py
@@ -2,13 +2,14 @@
 """ docstring """
 
 import argparse
-import os
-from itertools import combinations
+import sys
 import numpy as np
 import matplotlib
 
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
+
+import dcatools as tools
 
 plt.rcParams["figure.figsize"] = [10, 7.5]
 
@@ -17,13 +18,16 @@ def parse_options():
     """ cli parser """
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-s", "--msa", dest="msa", required=True, help="msa file"
+        "-n", "--msa", dest="msa", action='append', required=False, help="numerical msa file(s)"
     )
     parser.add_argument(
-        "-c", "--mcmc", dest="mcmc", action='append', required=True, help="mcmc file"
+        "-e", "--emergoes", dest="energies", action='append', required=False, help="msa energy file(s)"
     )
     parser.add_argument(
-        "-l", "--legend", dest="legend", action='append',  required=True, help="legends"
+        "-p", "--parameters", dest="params", required=False, help="msa energy file(s)"
+    )
+    parser.add_argument(
+        "-l", "--labels", dest="labels", action='append',  required=True, help="labels"
     )
     parser.add_argument(
         "-t", "--title", dest="title", required=True, help="title prefix"
@@ -41,65 +45,43 @@ def parse_options():
     return parser.parse_args()
 
 
-def load_energies(energy_file):
-    """ load sequences """
-    data = np.loadtxt(energy_file, dtype="double", skiprows=1)
-    return data
-
-
 def main():
     """ do stuff """
     options = parse_options()
 
-    msa_energies = load_energies(options.msa)
-    mcmc_files = options.mcmc
-    mcmc_energies = [load_energies(mcmc_file) for mcmc_file in mcmc_files]
-    mcmc_labels = options.legend
+    if options.energies is not None:
+        msa_energies = [tools.load_energies(msa_file) for msa_file in options.energies]
+    elif options.msa is not None and options.parameters is not None:
+        h, J = tools.load_model(options.parameters)
+        msa_energies = [tools.compute_energies(tools.load_sequences(msa_file), h, J) for msa_file in options.msa]
+    else:
+        sys.exit("ERROR: missing input data")
+
+    msa_labels = options.labels
 
     standard_energy = 0
     if options.mode == "1":
-        standard_energy = msa_energies[0]
+        standard_energy = msa_energies[0][0]
+        msa_energies = [msa_energy - standard_energy for msa_energy in msa_energies]
     elif options.mode == "2":
-        standard_energy = np.mean(msa_energies)
-
-    msa_energies = msa_energies - standard_energy
-    mcmc_energies = [mcmc_energy - standard_energy for mcmc_energy in mcmc_energies]
+        standard_energy = np.mean(msa_energies[0])
+        msa_energies = [msa_energy - standard_energy for msa_energy in msa_energies]
 
     with plt.style.context("fivethirtyeight"):
-        plt.hist(
-            msa_energies,
-            alpha=0.5,
-            label="MSA",
-            density=True,
-        )
-        for i, mcmc_energy in enumerate(mcmc_energies):
+        for i, msa_energy in enumerate(msa_energies):
             plt.hist(
-                mcmc_energy,
+                msa_energy,
                 alpha=0.5,
-                label=mcmc_labels[i],
+                label=msa_labels[i],
                 density=True,
             )
         plt.legend(loc="upper right")
         plt.xlabel("Sequence Energy")
-        plt.xlim(-270, -110)
+        #  plt.xlim(-270, -110)
         plt.ylabel("Probability")
         plt.title(options.title)
         plt.savefig(options.output)
         plt.close()
-    
-    #  with plt.style.context("fivethirtyeight"):
-    #      plt.hist([msa_energies] + mcmc_energies,
-    #          alpha=1,
-    #          label=["MSA"] + mcmc_labels,
-    #          density=True,
-    #      )
-    #      plt.legend(loc="upper right")
-    #      plt.xlabel("Sequence Energy")
-    #      plt.xlim(-270, -110)
-    #      plt.ylabel("Probability")
-    #      plt.title(options.title)
-    #      plt.savefig(options.output)
-    #      plt.close()
 
 
 if __name__ == "__main__":
