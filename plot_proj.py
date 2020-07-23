d294f6c2d0dbdb07e25ed6a8bf050df857d09b24
sudorook <daemon@nullcodon.com>
2020-07-22 20:42:46 -0500

    update plot_proj

--- plot_proj.py
+++ plot_proj.py
@@ -10,12 +10,11 @@ import matplotlib
 
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
-import seaborn as sns
+
 from pysca import scaTools as sca
+import dcatools as tools
 
 plt.rcParams["figure.figsize"] = [10, 7.5]
-sns.set(rc={"figure.figsize": (10, 7.5)})
-
 
 random.seed(0)
 np.random.seed(0)
@@ -25,35 +24,27 @@ def parse_options():
     """ cli parser """
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-s",
-        "--msa_input",
-        dest="msa",
-        required=True,
-        help="numerical msa alignment",
-    )
-    parser.add_argument(
-        "-r",
-        "--mcmc_reg",
-        dest="mcmc_reg",
-        required=True,
-        help="numerical mcmc alignment (reg)",
-    )
-    parser.add_argument(
         "-n",
-        "--mcmc_new",
-        dest="mcmc_new",
+        "--msa_input",
+        dest="msa_basis",
         required=True,
-        help="numerical mcmc alignment (new)",
+        help="numerical msa alignment for basis",
     )
     parser.add_argument(
-        "-o",
-        "--mcmc_old",
-        dest="mcmc_old",
+        "-N",
+        "--msa_proj",
+        dest="msa_proj",
         required=True,
-        help="numerical mcmc alignment (old)",
+        help="numerical msa to project",
     )
     parser.add_argument(
-        "-w", "--weights", dest="weights", required=True, help="msa weights"
+        "-w", "--weights_input", dest="weights_input", help="msa basis weights"
+    )
+    parser.add_argument(
+        "-W", "--weights_proj", dest="weights_proj", help="msa projection weights"
+    )
+    parser.add_argument(
+        "-t", "--title", dest="title", help="figure title"
     )
     return parser.parse_args()
 
@@ -64,13 +55,13 @@ def load_sequences(seq_file):
     return data
 
 
-def load_sequence_weights(weight_file):
-    """ load msa weights """
+def load_sequence_weights_input(weight_file):
+    """ load msa weights_input """
     return np.loadtxt(weight_file, dtype="float")
 
 
-def compute_sequence_weights(msa, Q=21):
-    """ comptue seq weights """
+def compute_sequence_weights_input(msa, Q=21):
+    """ comptue seq weights_input """
     X2d = sca.alg2bin(msa, Q)
     simMat = (X2d.dot(X2d.T)).todense() / msa.shape[1]
     seqw = np.array(1 / (simMat > 0.8).sum(axis=0))
@@ -109,12 +100,12 @@ def alg2bin(alg, N_aa=21):
     return Abin
 
 
-def sample_msa_weighted(msa, weights, N):
+def sample_msa_weighted(msa, weights_input, N):
     """ subset the alignment """
-    Ntot = min(int(sum(weights)), N)
-    wlist = [w for w in weights]
+    Ntot = min(int(sum(weights_input)), N)
+    wlist = [w for w in weights_input]
     selection2 = np.random.choice(
-        range(msa.shape[0]), Ntot, replace=False, p=weights / sum(weights)
+        range(msa.shape[0]), Ntot, replace=False, p=weights_input / sum(weights_input)
     )
     return msa[selection2, :]
 
@@ -123,30 +114,45 @@ def main():
     """ do stuff """
     options = parse_options()
 
-    msa = load_sequences(options.msa) + 1
-    mcmc_new = load_sequences(options.mcmc_new) + 1
-    mcmc_old = load_sequences(options.mcmc_old) + 1
-    mcmc_reg = load_sequences(options.mcmc_reg) + 1
+    msa_basis = load_sequences(options.msa_basis) + 1
+    msa_proj = load_sequences(options.msa_proj) + 1
 
-    msa_weights = load_sequence_weights(options.weights)
+    outfile_basis = "msa_basis_" + tools.slugify(options.title) + ".png"
+    outfile_proj = "msa_proj_" + tools.slugify(options.title) + ".png"
 
-    # too many sequences, so subset the alignment and recompute the weights
-    msa_subset = sample_msa_weighted(msa, msa_weights, 25000)
-    msa_subset_2 = sample_msa_weighted(msa, msa_weights, 10000)
-    msa_subset_weights = compute_sequence_weights(msa_subset)
+    [M, N] = msa_basis.shape
+    if options.weights_input is not None:
+        msa_weights_basis = load_sequence_weights_input(options.weights_input)
+    else:
+        msa_weights_basis = np.ones(M)
+    
+    [M2, N2] = msa_proj.shape
+    if options.weights_proj is not None:
+        msa_weights_proj = load_sequence_weights_input(options.weights_proj)
+    else:
+        msa_weights_proj = np.ones(M2)
+
+    if M > 10000:
+        # too many sequences, so subset the alignment and recompute the
+        # weights_input
+        print("subsetting the alignment...")
+        msa_basis = sample_msa_weighted(msa_basis, msa_weights_basis, 10000)
+        msa_weights_basis = compute_sequence_weights_input(msa_basis)
 
     # compute eignvectors for the msa
-    msa_subset_sim = compute_sim_mat(msa_subset)
-    posw, Dia, Di = sca.posWeights(
-        msa_subset, msa_subset_weights, 0, 21, np.ones(21) / 21
-    )
-    X2d = sca.alg2bin(msa_subset, 21)
-    X2d_2 = sca.alg2bin(msa_subset_2, 21)
-    X2dw = sparsify(np.diag(np.sqrt(msa_subset_weights[0]))).dot(X2d)
+    msa_sim = compute_sim_mat(msa_basis)
+    #  posw, Dia, Di = sca.posWeights(
+    #      msa_basis, msa_weights_basis, 0, 21, np.ones(21) / 21
+    #  )
+    X2d = sca.alg2bin(msa_basis, 21)
+    #  X2dw = sparsify(np.diag(np.sqrt(msa_weights_basis[0]))).dot(X2d)
+    X2dw = sparsify(np.diag(np.sqrt(msa_weights_basis))).dot(X2d)
     u, s, v = sca.svdss(X2dw, k=250)
-    print(s)
-    print((s[0] ** 2 + s[1] ** 2) / sum(s ** 2))
-    tmp = X2d_2.dot(v).dot(np.diag(1 / s))
+
+    #  print(s)
+    #  print((s[0] ** 2 + s[1] ** 2) / sum(s ** 2))
+
+    tmp = X2d.dot(v).dot(np.diag(1 / s))
     data_df = pd.DataFrame(data={"PC1": tmp[:, 0], "PC2": tmp[:, 1]})
 
     # plot the projection of the msa onto the first two eigenvectors
@@ -158,11 +164,11 @@ def main():
         [top, bottom] = ax.get_ylim()
         plt.title("Projection of MSA samples onto top eigenvectors")
         plt.tight_layout()
-        plt.savefig("msa_proj.svg")
+        plt.savefig(outfile_basis)
         plt.close()
 
     # project new mcmc onto msa eigenvectors
-    X2d_new = sca.alg2bin(mcmc_new, 21)
+    X2d_new = sca.alg2bin(msa_proj, 21)
     tmp_new = X2d_new.dot(v).dot(np.diag(1 / s))
 
     data_df_new = pd.DataFrame(
@@ -177,45 +183,7 @@ def main():
         ax.set_xlim(left, right)
         ax.set_ylim(top, bottom)
         plt.tight_layout()
-        plt.savefig("mcmc_new_proj.svg")
-        plt.close()
-
-    # project reg mcmc onto msa eigenvectors
-    X2d_reg = sca.alg2bin(mcmc_reg, 21)
-    tmp_reg = X2d_reg.dot(v).dot(np.diag(1 / s))
-
-    data_df_reg = pd.DataFrame(
-        data={"PC1": tmp_reg[:, 0], "PC2": tmp_reg[:, 1]}
-    )
-
-    with plt.style.context("fivethirtyeight"):
-        ax = data_df_reg.plot.hexbin(
-            x="PC1", y="PC2", mincnt=1, cmap="viridis", zorder=1
-        )
-        plt.title("Projection of reg MCMC onto MSA space")
-        ax.set_xlim(left, right)
-        ax.set_ylim(top, bottom)
-        plt.tight_layout()
-        plt.savefig("mcmc_reg_proj.svg")
-        plt.close()
-
-    # project old mcmc onto msa eigenvectors
-    X2d_old = sca.alg2bin(mcmc_old, 21)
-    tmp_old = X2d_old.dot(v).dot(np.diag(1 / s))
-
-    data_df_old = pd.DataFrame(
-        data={"PC1": tmp_old[:, 0], "PC2": tmp_old[:, 1]}
-    )
-
-    with plt.style.context("fivethirtyeight"):
-        ax = data_df_old.plot.hexbin(
-            x="PC1", y="PC2", mincnt=1, cmap="viridis", zorder=1
-        )
-        plt.title("Projection of old MCMC onto MSA space")
-        ax.set_xlim(left, right)
-        ax.set_ylim(top, bottom)
-        plt.tight_layout()
-        plt.savefig("mcmc_old_proj.svg")
+        plt.savefig(outfile_proj)
         plt.close()
 
 
