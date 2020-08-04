c7730fcbd47f6f700bb99fa4823417bafc68862a
sudorook <daemon@nullcodon.com>
2020-08-04 14:23:18 -0500

    stop plotting sorted energies

--- plot_energy_compare.py
+++ plot_energy_compare.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 
 import dcatools as tools
 
-plt.rcParams["figure.figsize"] = [10, 15]
+plt.rcParams["figure.figsize"] = [10, 8]
 
 
 def parse_options():
@@ -65,30 +65,28 @@ def main():
     energies1 = tools.load_energies(options.msa)
     energies2 = tools.load_energies(options.mcmc)
 
-    energies1 = energies1 - energies1[0]
-    energies2 = energies2 - energies2[0]
-
-    energies1_sorted = np.sort(energies1)
-    energies2_sorted = np.sort(energies2)
+    #  energies1_sorted = np.sort(energies1)
+    #  energies2_sorted = np.sort(energies2)
 
     df_e = pd.DataFrame(data={"e1": energies1, "e2": energies2,})
     res_e = sm.ols(formula="e2 ~ e1", data=df_e).fit()
     params_e = res_e.params
 
-    df_es = pd.DataFrame(
-        data={"e1": energies1_sorted, "e2": energies2_sorted,}
-    )
-    res_es = sm.ols(formula="e2 ~ e1", data=df_es).fit()
-    params_es = res_es.params
+    #  df_es = pd.DataFrame(
+    #      data={"e1": energies1_sorted, "e2": energies2_sorted,}
+    #  )
+    #  res_es = sm.ols(formula="e2 ~ e1", data=df_es).fit()
+    #  params_es = res_es.params
 
     with plt.style.context("fivethirtyeight"):
-        fig, ax = plt.subplots(2, 1)
+        #  fig, ax = plt.subplots(2, 1)
+        fig, ax = plt.subplots(1, 1)
 
-        ax[0].scatter(x=energies1, y=energies2, color="midnightblue", s=5)
-        x = np.linspace(*(ax[0]).get_xlim())
+        ax.scatter(x=energies1, y=energies2, color="midnightblue", s=5)
+        x = np.linspace(*(ax).get_xlim())
 
-        ax[0].plot(x, x, "--k", alpha=0.25, zorder=0)
-        ax[0].plot(
+        ax.plot(x, x, "--k", alpha=0.25, zorder=0)
+        ax.plot(
             x,
             params_e.e1 * x + params_e.Intercept,
             label=r"y={:.3g}x+{:.3g}, $R^2$={:.3g}".format(
@@ -97,27 +95,44 @@ def main():
             alpha=0.5,
         )
 
-        ax[0].set_xlabel(options.msa_label)
-        ax[0].set_ylabel(options.mcmc_label)
-        ax[0].legend(loc="lower right")
+        ax.set_xlabel(options.msa_label)
+        ax.set_ylabel(options.mcmc_label)
+        ax.legend(loc="lower right")
 
-        ax[1].scatter(
-            x=energies1_sorted, y=energies2_sorted, color="midnightblue", s=5
-        )
-        x = np.linspace(*(ax[1]).get_xlim())
+        #  ax[0].scatter(x=energies1, y=energies2, color="midnightblue", s=5)
+        #  x = np.linspace(*(ax[0]).get_xlim())
+        #
+        #  ax[0].plot(x, x, "--k", alpha=0.25, zorder=0)
+        #  ax[0].plot(
+        #      x,
+        #      params_e.e1 * x + params_e.Intercept,
+        #      label=r"y={:.3g}x+{:.3g}, $R^2$={:.3g}".format(
+        #          params_e.e1, params_e.Intercept, res_e.rsquared
+        #      ),
+        #      alpha=0.5,
+        #  )
+        #
+        #  ax[0].set_xlabel(options.msa_label)
+        #  ax[0].set_ylabel(options.mcmc_label)
+        #  ax[0].legend(loc="lower right")
 
-        ax[1].plot(x, x, "--k", alpha=0.25, zorder=0)
-        ax[1].plot(
-            x,
-            params_es.e1 * x + params_es.Intercept,
-            label=r"y={:.3g}x+{:.3g}, $R^2$={:.3g}".format(
-                params_es.e1, params_es.Intercept, res_es.rsquared
-            ),
-            alpha=0.5,
-        )
-        ax[1].set_xlabel(options.msa_label + " (sorted)")
-        ax[1].set_ylabel(options.mcmc_label + " (sorted)")
-        ax[1].legend(loc="lower right")
+        #  ax[1].scatter(
+        #      x=energies1_sorted, y=energies2_sorted, color="midnightblue", s=5
+        #  )
+        #  x = np.linspace(*(ax[1]).get_xlim())
+        #
+        #  ax[1].plot(x, x, "--k", alpha=0.25, zorder=0)
+        #  ax[1].plot(
+        #      x,
+        #      params_es.e1 * x + params_es.Intercept,
+        #      label=r"y={:.3g}x+{:.3g}, $R^2$={:.3g}".format(
+        #          params_es.e1, params_es.Intercept, res_es.rsquared
+        #      ),
+        #      alpha=0.5,
+        #  )
+        #  ax[1].set_xlabel(options.msa_label + " (sorted)")
+        #  ax[1].set_ylabel(options.mcmc_label + " (sorted)")
+        #  ax[1].legend(loc="lower right")
 
         fig.suptitle(options.msa_label + " vs " + options.mcmc_label)
 
