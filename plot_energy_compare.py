f70163871b12fe2cd7158c7885a20c889eef006f
sudorook <daemon@nullcodon.com>
2020-08-04 12:12:19 -0500

    add regression line to energy compare script

--- plot_energy_compare.py
+++ plot_energy_compare.py
@@ -5,13 +5,15 @@ import argparse
 import os
 import numpy as np
 import matplotlib
+import pandas as pd
+import statsmodels.formula.api as sm
 
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
 import dcatools as tools
 
-plt.rcParams["figure.figsize"] = [10, 12]
+plt.rcParams["figure.figsize"] = [10, 15]
 
 
 def parse_options():
@@ -69,20 +71,53 @@ def main():
     energies1_sorted = np.sort(energies1)
     energies2_sorted = np.sort(energies2)
 
+    df_e = pd.DataFrame(data={"e1": energies1, "e2": energies2,})
+    res_e = sm.ols(formula="e2 ~ e1", data=df_e).fit()
+    params_e = res_e.params
+
+    df_es = pd.DataFrame(
+        data={"e1": energies1_sorted, "e2": energies2_sorted,}
+    )
+    res_es = sm.ols(formula="e2 ~ e1", data=df_es).fit()
+    params_es = res_es.params
+
     with plt.style.context("fivethirtyeight"):
         fig, ax = plt.subplots(2, 1)
 
-        ax[0].scatter(x=energies1, y=energies2)
+        ax[0].scatter(x=energies1, y=energies2, color="midnightblue", s=5)
         x = np.linspace(*(ax[0]).get_xlim())
+
         ax[0].plot(x, x, "--k", alpha=0.25, zorder=0)
+        ax[0].plot(
+            x,
+            params_e.e1 * x + params_e.Intercept,
+            label=r"y={:.3g}x+{:.3g}, $R^2$={:.3g}".format(
+                params_e.e1, params_e.Intercept, res_e.rsquared
+            ),
+            alpha=0.5,
+        )
+
         ax[0].set_xlabel(options.msa_label)
         ax[0].set_ylabel(options.mcmc_label)
+        ax[0].legend(loc="lower right")
 
-        ax[1].scatter(x=energies1_sorted, y=energies2_sorted)
-        x = np.linspace(*ax[1].get_xlim())
+        ax[1].scatter(
+            x=energies1_sorted, y=energies2_sorted, color="midnightblue", s=5
+        )
+        x = np.linspace(*(ax[1]).get_xlim())
+
         ax[1].plot(x, x, "--k", alpha=0.25, zorder=0)
+        ax[1].plot(
+            x,
+            params_es.e1 * x + params_es.Intercept,
+            label=r"y={:.3g}x+{:.3g}, $R^2$={:.3g}".format(
+                params_es.e1, params_es.Intercept, res_es.rsquared
+            ),
+            alpha=0.5,
+        )
         ax[1].set_xlabel(options.msa_label + " (sorted)")
         ax[1].set_ylabel(options.mcmc_label + " (sorted)")
+        ax[1].legend(loc="lower right")
 
         fig.suptitle(options.msa_label + " vs " + options.mcmc_label)
 
