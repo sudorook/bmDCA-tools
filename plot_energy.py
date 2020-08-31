43d1d2d6193d33c5fd2588c8f6a67efc283b9477
sudorook <daemon@nullcodon.com>
2020-08-31 00:40:45 -0500

    enable latex-ish title syntax

--- plot_energy.py
+++ plot_energy.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 import dcatools as tools
 
 plt.rcParams["figure.figsize"] = [12, 9]
+plt.rcParams.update({'mathtext.default':  'regular' })
 
 
 def parse_options():
