076d6522aae7f333ad5e90833dc9e51f8b16eb8b
sudorook <daemon@nullcodon.com>
2020-09-09 20:17:00 -0500

    add flag for sorting energies before comparing

--- plot_energy_compare.py
+++ plot_energy_compare.py
@@ -65,6 +65,14 @@ def parse_options():
         default=0,
         help="offset type (default none)",
     )
+    parser.add_argument(
+        "-s",
+        "--sort",
+        dest="sort",
+        action="store_true",
+        default=False,
+        help="sort energies before comparing",
+    )
     return parser.parse_args()
 
 
@@ -90,33 +98,24 @@ def main():
     if len(energies) != 2:
         sys.exit("ERROR: only comparison of 2 energies supported.")
 
-    #  prefix1 = os.path.splitext((options.msa).replace("/", "_"))[0]
-    #  prefix2 = os.path.splitext((options.mcmc).replace("/", "_"))[0]
-
-    #  energies1 = tools.load_energies(options.msa)
-    #  energies2 = tools.load_energies(options.mcmc)
-
-    #  energies1_sorted = np.sort(energies1)
-    #  energies2_sorted = np.sort(energies2)
-
-    #  df_e = pd.DataFrame(data={"e1": energies1, "e2": energies2,})
-    df_e = pd.DataFrame(
-        data={
-            "e1": energies[0],
-            "e2": energies[1],
-        }
-    )
+    if options.sort:
+        df_e = pd.DataFrame(
+            data={
+                "e1": np.sort(energies[0]),
+                "e2": np.sort(energies[1]),
+            }
+        )
+    else:
+        df_e = pd.DataFrame(
+            data={
+                "e1": energies[0],
+                "e2": energies[1],
+            }
+        )
     res_e = sm.ols(formula="e2 ~ e1", data=df_e).fit()
     params_e = res_e.params
 
-    #  df_es = pd.DataFrame(
-    #      data={"e1": energies1_sorted, "e2": energies2_sorted,}
-    #  )
-    #  res_es = sm.ols(formula="e2 ~ e1", data=df_es).fit()
-    #  params_es = res_es.params
-
     with plt.style.context("fivethirtyeight"):
-        #  fig, ax = plt.subplots(2, 1)
         fig, ax = plt.subplots(1, 1)
 
         ax.scatter(x=energies[0], y=energies[1], color="midnightblue", s=5)
@@ -136,48 +135,12 @@ def main():
         ax.set_ylabel(options.labels[1])
         ax.legend(loc="lower right")
 
-        #  ax[0].scatter(x=energies1, y=energies2, color="midnightblue", s=5)
-        #  x = np.linspace(*(ax[0]).get_xlim())
-        #
-        #  ax[0].plot(x, x, "--k", alpha=0.25, zorder=0)
-        #  ax[0].plot(
-        #      x,
-        #      params_e.e1 * x + params_e.Intercept,
-        #      label=r"y={:.3g}x+{:.3g}, $R^2$={:.3g}".format(
-        #          params_e.e1, params_e.Intercept, res_e.rsquared
-        #      ),
-        #      alpha=0.5,
-        #  )
-        #
-        #  ax[0].set_xlabel(options.msa_label)
-        #  ax[0].set_ylabel(options.mcmc_label)
-        #  ax[0].legend(loc="lower right")
-
-        #  ax[1].scatter(
-        #      x=energies1_sorted, y=energies2_sorted, color="midnightblue", s=5
-        #  )
-        #  x = np.linspace(*(ax[1]).get_xlim())
-        #
-        #  ax[1].plot(x, x, "--k", alpha=0.25, zorder=0)
-        #  ax[1].plot(
-        #      x,
-        #      params_es.e1 * x + params_es.Intercept,
-        #      label=r"y={:.3g}x+{:.3g}, $R^2$={:.3g}".format(
-        #          params_es.e1, params_es.Intercept, res_es.rsquared
-        #      ),
-        #      alpha=0.5,
-        #  )
-        #  ax[1].set_xlabel(options.msa_label + " (sorted)")
-        #  ax[1].set_ylabel(options.mcmc_label + " (sorted)")
-        #  ax[1].legend(loc="lower right")
-
         if options.title is None:
             plt.title(options.labels[0] + " vs " + options.labels[1])
         else:
             plt.title(options.title)
 
         plt.tight_layout()
-        #  plt.savefig(prefix1 + "_" + prefix2 + "_energies.png")
         plt.savefig(options.output)
         plt.close()
 
